d6df1df7fe95967a85456b8a0ddc5ec6c9d48fb9
neko-shinshi <claude.hjw@gmail.com>
2023-09-18 15:38:31 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  i18n: {
-    "locales": ["default", "en", "ja"],
-    "defaultLocale": "default",
-    localeDetection: false
-  }
 }
 
 module.exports = nextConfig
